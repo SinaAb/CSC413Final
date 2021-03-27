fde80eeac8f550207fd0ea217be541a0bcd95035
Sina Abady <sinaabady@gmail.com>
2021-03-27 16:02:01 -0400

    barebones dependancies installation

---
+++ dependancies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip3 install tensorflow
+pip3 install gym
+pip3 install gym-retro
+
+echo "------------------------------------ Python Versions of Base Developer -------------------------------------"
+echo "$(cat versions.txt)"
