6f4c4210b91b214f26e02a216c934c52b47bab0f
Sina Abady <sinaabady@gmail.com>
2021-03-29 12:43:37 -0400

    add ReplayBuffer class

--- DQN/DQNAgent.py
+++ DQN/DQNAgent.py
@@ -4,11 +4,13 @@ from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import TensorBoard
-from collections import deque
+from collections import namedtuple, deque
 import numpy as np
+import random
 import time
 
-# Cusotm tensorboard class for log writing so we dont have 10 million logs haha
+
+# Custom tensorboard class for log writing so we dont have 10 million logs haha
 class ModifiedTensorBoard(TensorBoard):
 
     # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
@@ -40,11 +42,35 @@ class ModifiedTensorBoard(TensorBoard):
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
 
+# the Replay Memory for training the target network
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size):
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+    def add(self, state, action, reward, next_state, done):
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = np.array(e.state for e in experiences if e is not None)
+        actions = np.array([e.action for e in experiences if e is not None])
+        rewards = np.array([e.reward for e in experiences if e is not None])
+        next_states = np.array([e.next_state for e in experiences if e is not None])
+        dones = np.array([e.done for e in experiences if e is not None])
+
+        return states, actions, rewards, next_states, dones
+
+    def __len__(self):
+        return len(self.memory)
 
 class DQNAgent:
-    # initalize the DQN agent which trains a convultional model given pixel data of the game
-    def __init__(self, env, replay_memory_size=50000, min_replay_memory_size=500):
-        self.env = env  # game environment
+    # initialize the DQN agent which trains a convolution model given pixel data of the game
+    def __init__(self, environment, buffer_size=50000, batch_size=128, replay_every=32):
+        self.env = environment  # game environment
         self.model = self.create_model()  # the DQN network
         self.model_name = "DQN: " + self.env.gamename + "\nSize: " + str(self.env.observation_space.shape)
 
@@ -52,12 +78,11 @@ class DQNAgent:
         # stochastic. We will then incrementally update this model using the true model after some N training steps.
         self.target_model = self.create_model()
         self.target_model.set_weights(self.model.get_weights())  # copy the true model
-        self.target_update_counter = 0  # counts number of training steps taken used to update the target model
 
         # replay memory initialization
-        self.replay_memory_size = replay_memory_size  # total number of training steps to store
-        self.min_replay_memory_size = min_replay_memory_size  # minimum number of steps to replay for model training
-        self.replay_memory = deque(maxlen=self.replay_memory_size)  # create a queue for replay memory
+        self.memory = ReplayBuffer(buffer_size, batch_size)
+        self.replay_every = replay_every # number of training iterations taken before the target model is updated
+        self.time_step = 0
 
         # our TensorBoard for writing to one log for every time we call model.fit()
         self.tensorboard = ModifiedTensorBoard(log_dir=f'logs/{self.model_name}-{int(time.time())}')
@@ -68,6 +93,22 @@ class DQNAgent:
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.01
+
+    def step(self, state, action, reward, next_state, done):
+        # Save the experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+
+        # update the target model once we reach replay_every time steps taken
+        self.time_step = (self.time_step + 1) % self.replay_every
+        if self.time_step == 0 and len(self.memory) > self.memory.batch_size:
+            experiences = self.memory.sample()
+            self.learn(experiences)
+
+    def act(self, state):
+        if random.uniform(0, 1) < self.epsilon:
+
+    def train(self, experiences):
+
 
     # Creates a CNN with two CONV2D layers with ReLU, Pooling, and Dropout.
     # Data is finally passed through 2 Dense layers which outputs a sigmoid activated output of action probabilities
@@ -92,10 +133,6 @@ class DQNAgent:
 
         return model
 
-    # Adds step's data to a memory replay array
-    # (observation space, action, reward, new observation space, done)
-    def update_replay_memory(self, state, action, reward, new_state, done):
-        self.replay_memory.append([state, action, reward, new_state, done])
 
     def get_action_meanings(self):
         meaning = ""
@@ -107,23 +144,23 @@ class DQNAgent:
 
         return meaning
 
-    def random_run(self):
-        self.env.reset()
-        while True:
-            obs, rew, done, info = self.env.step(self.env.action_space.sample())
-            self.env.render()
-            if done:
-                break
 
-            if rew > 0:
-                print("Reward:", rew)
+def random_run(self, env):
+    env.reset()
+    while True:
+        obs, rew, done, info = env.step(env.action_space.sample())
+        env.render()
+        if done:
+            break
 
-            time.sleep(0.0166)
-        self.env.close()
+        if rew > 0:
+            print("Reward:", rew)
 
+        time.sleep(0.0166)
+    env.close()
 
 if __name__ == '__main__':
-    env = retro.make(game='Airstriker-Genesis')
-
-    agent = DQNAgent(env)
-    agent.random_run()
+    # env = retro.make(game='Airstriker-Genesis')
+    #
+    # agent = DQNAgent(env)
+    # agent.random_run()
